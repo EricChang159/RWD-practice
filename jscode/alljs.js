1f12c64e5ffaeef815c653b847afc8b601ffb489
ERIC <eric.chang159@gmail.com>
2019-08-16 03:51:54 +0800

    add footer icon imporve the RWD

--- jscode/alljs.js
+++ jscode/alljs.js
@@ -1,9 +1,19 @@
 $(function () {
+    $('.head-logo,.footer-logo').on('click', function () {
+        $('html,body').animate({
+            scrollTop: $('#page-one').offset().top
+        }, 500)
+    })
+})
+
+$(function () {
     $('a[href="#page-two"]').click(function () {
         $("html,body").animate({
-            scrollTop: $("#page-two").offset().top
+            scrollTop: 750
+            // page two position 750
         }, 1250)
     })
+    $()
 })
 $(function () {
     $('a[href="#page-three"]').click(function () {
@@ -12,6 +22,15 @@ $(function () {
         }, 1250)
     })
 })
+$(function () {
+    $('a[href="#footer"]').click(function () {
+        $("html,body").animate({
+            scrollTop: $("#footer").offset().top
+        }, 1250)
+    })
+})
+
+// 頁面跳轉動畫
 
 $(function () {
     var $headerBar = $('.pageone-header-bar')
@@ -36,3 +55,60 @@ $(function () {
         }
     })
 })
+// headbar 樣式更換動畫
+// scroll捲動觸發事件， scrollTop是當下位置，會一直變， offset 是取當下位置.top .left可得值
+// offset({top:y, left:x})可用來設定
+
+
+$(function () {
+    $('.header-icon').on('click', function (e) {
+        e.preventDefault();
+        $('.mobile-menu').toggle(300)
+    })
+    $('.mobile-menu').on('mouseleave', function (e) {
+        $('.mobile-menu').hide(300)
+    })
+})
+// mouseout太敏感了 需要使用mouseleave才可以 兩者差在冒泡與否，事件綁定的層度不同
+// menu 動畫
+
+$(function () {
+    var $toFirstPage = $('.to-firstPage')
+    $toFirstPage.on('click', function () {
+        $('html,body').animate({
+            scrollTop: $('#page-one').offset().top
+        }, 500)
+    })
+
+    $(window).on('scroll', throttle(function () {
+        // console.log($toFirstPage.offset(),'1')
+        if ($toFirstPage.offset().top > 1170) {
+            $toFirstPage.addClass('show-to-firstPage')
+        } else {
+            $toFirstPage.removeClass('show-to-firstPage')
+        }
+        console.log(1)
+    }, 10))
+})
+// 右下角小按鈕動畫
+
+function throttle(func, settime) {
+    var last, timer;
+    if (settime) settime = 250;
+    return function () {
+        var context = this;
+        var args = arguments
+        var now = +new Date()
+        if (last && now < last + settime) {
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                last = now;
+                func.apply(context, args);
+            }, settime)
+        } else {
+            last = now;
+            func.apply(context, args);
+        }
+    }
+}
+// throttle 減少效能浪費 與debounce 一樣功能但是最後會在觸發一次
